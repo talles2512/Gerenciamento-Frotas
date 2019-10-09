ae8b2300f222f0fb1b6edb0204effd5a62ab00b7
Leonardo C. Oliveira <Leonardo C. Oliveira@DESKTOP-N5MLJ6D>
2019-10-09 15:10:10 -0300

    Alteração tipos BIGINT DB_GERENCFROTA. Alteração Querys Motorista

--- CamadaModelagem/Data/MotoristaDAL.cs
+++ CamadaModelagem/Data/MotoristaDAL.cs
@@ -22,8 +22,8 @@ namespace CamadaModelagem.Data
 
         public bool Cadastrar(Motorista motorista, CNH cnh) //Mudança na Query, Verificar
         {
-            string query = "INSERT INTO[dbo].[TB_MOTORISTA]([MT_CPF],[MT_NOME],[MT_RG],[MT_ENDERECO],[MT_DTNASCIMENTO],[MT_TELEFONE],[MT_TELEFONE_RECADO],[MT_SITUACAO],[MT_CNH_NUMERO])" +
-                "VALUES ('" + motorista.CPF + "', '" + motorista.Name + "', '" + motorista.RG + "', '" + motorista.Endereco + "', '" + motorista.DataNascimento + "', " + motorista.Telefone + ", " + motorista.TelefoneContato + ", " + motorista.Situacao + "," + cnh.Numero + ") GO" +
+            string query = "INSERT INTO[dbo].[TB_MOTORISTA]([MT_CPF],[MT_NOME],[MT_RG],[MT_ENDERECO],[MT_DTNASCIMENTO],[MT_TELEFONE],[MT_TELEFONE_RECADO],[MT_SITUACAO])" +
+                "VALUES ('" + motorista.CPF + "', '" + motorista.Name + "', '" + motorista.RG + "', '" + motorista.Endereco + "', '" + motorista.DataNascimento + "', " + motorista.Telefone + ", " + motorista.TelefoneContato + ", " + motorista.Situacao + ") GO" +
                 "INSERT INTO [dbo].[TB_CNH]([CNH_NUMERO],[CNH_DTEMISSAO],[CNH_DTVENC],[CNH_CATEGORIA],[CNH_ORGAOEMISSOR],[CNH_MT_CPF])" +
                 "VALUES (" + cnh.Numero + ", '" + cnh.DataEmissao + "', '" + cnh.DataVencimento + "', '" + cnh.Categoria + "', '" + cnh.OrgaoEmissor + "', '" + motorista.CPF + "')";
             try
@@ -44,7 +44,7 @@ namespace CamadaModelagem.Data
 
         public bool Alterar(Motorista motorista, CNH cnh, string cpf) //Mudança na Query, Verificar
         {
-            string Query = "UPDATE [TB_MOTORISTA] SET [MT_CPF] ='" + motorista.CPF + "', [MT_NOME] = '" + motorista.Name + "', [MT_RG] = '" + motorista.RG + "', [MT_ENDERECO] = '" + motorista.RG + "', [MT_ENDERECO] ='" + motorista.Endereco + "', [MT_DTNASCIMENTO] = '" + motorista.DataNascimento + "',[MT_TELEFONE] = " + motorista.Telefone + ", [MT_TELEFONE_RECADO] = " + motorista.TelefoneContato + ", [MT_SITUACAO] = " + motorista.Situacao + ", [MT_CNH_NUMERO] =" + cnh.Numero + " WHERE [MT_CPF] = " + cpf + "; GO" +
+            string Query = "UPDATE [TB_MOTORISTA] SET [MT_CPF] ='" + motorista.CPF + "', [MT_NOME] = '" + motorista.Name + "', [MT_RG] = '" + motorista.RG + "', [MT_ENDERECO] = '" + motorista.RG + "', [MT_ENDERECO] ='" + motorista.Endereco + "', [MT_DTNASCIMENTO] = '" + motorista.DataNascimento + "',[MT_TELEFONE] = " + motorista.Telefone + ", [MT_TELEFONE_RECADO] = " + motorista.TelefoneContato + ", [MT_SITUACAO] = " + motorista.Situacao + ",  WHERE [MT_CPF] = " + cpf + "; GO" +
                 "UPDATE [TB_CNH] SET [CNH_NUMERO] =" + cnh.Numero + ", [CNH_DTEMISSAO] = '" + cnh.DataEmissao + "', [CNH_DTVENC] '" + cnh.DataVencimento + "', [CNH_CATEGORIA] ='" + cnh.Categoria + "', [CNH_ORGAOEMISSOR] = '" + cnh.OrgaoEmissor + "' , [CNH_MT_CPF] = " + motorista.CPF + " WHERE [CNH_MT_CPF] = '" + cpf +"'";
             return _banco.ExecutarInstrucao(Query);
         }
@@ -52,7 +52,7 @@ namespace CamadaModelagem.Data
         public Motorista BuscarCPF(string cpf)
         {
             string query = "SELECT M.[MT_CPF], M.[MT_NOME], M.[MT_RG], M.[MT_ENDERECO], M.[MT_DTNASCIMENTO], M.[MT_TELEFONE], M.[MT_TELEFONE_RECADO]" +
-                ", M.[MT_SITUACAO], M.[MT_CNH_NUMERO], C.[CNH_NUMERO], C.[CNH_DTEMISSAO], C.[CNH_DTVENC], C.[CNH_CATEGORIA], C.[CNH_ORGAOEMISSOR], C.[CNH_MT_CPF]" +
+                ", M.[MT_SITUACAO], C.[CNH_NUMERO], C.[CNH_DTEMISSAO], C.[CNH_DTVENC], C.[CNH_CATEGORIA], C.[CNH_ORGAOEMISSOR], C.[CNH_MT_CPF]" +
                 "FROM [TB_MOTORISTA] AS M JOIN [TB_CNH] AS C ON M.[MT_CPF] = C.[CNH_MT_CPF] WHERE M.[MT_CPF] = '" + cpf + "'";
             DataTable dt = _banco.BuscarRegistro(query);
             Motorista motorista = null;
@@ -78,7 +78,7 @@ namespace CamadaModelagem.Data
         {
             List<Motorista> motoristas = new List<Motorista>();
             string query = "SELECT M.[MT_CPF], M.[MT_NOME], M.[MT_RG], M.[MT_ENDERECO], M.[MT_DTNASCIMENTO], M.[MT_TELEFONE], M.[MT_TELEFONE_RECADO]" +
-                ", M.[MT_SITUACAO], M.[MT_CNH_NUMERO], C.[CNH_NUMERO], C.[CNH_DTEMISSAO], C.[CNH_DTVENC], C.[CNH_CATEGORIA], C.[CNH_ORGAOEMISSOR], C.[CNH_MT_CPF]" +
+                ", M.[MT_SITUACAO], C.[CNH_NUMERO], C.[CNH_DTEMISSAO], C.[CNH_DTVENC], C.[CNH_CATEGORIA], C.[CNH_ORGAOEMISSOR], C.[CNH_MT_CPF]" +
                 "FROM [TB_MOTORISTA] AS M JOIN [TB_CNH] AS C ON M.[MT_CPF] = C.[CNH_MT_CPF]";
             DataTable dt = _banco.BuscarRegistro(query);
             Motorista motorista = null;
